7a83b0933a5045e3ae278d5772ec7b5ab72e384a
paola <paola.caric@gmail.com>
2023-08-01 14:48:37 +0200

    Initial Commit

---
+++ projekt1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projekt1
+//
+//  Created by Paola Caric on 01.08.2023..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
